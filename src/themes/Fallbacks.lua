5b1976651c2de952baa52a3073300a4a21223735
Zxcxresl <leonarzubelarry2@gmail.com>
2025-11-10 16:59:46 -0500

    Add files via upload

---
+++ src/themes/Fallbacks.lua
@@ -0,0 +1,42 @@
+return {
+    -- More soon!
+    
+    Dialog = "Accent",
+    
+    Background = "Accent",
+    BackgroundTransparency = 0,
+    Hover = "Text",
+    
+    WindowBackground = "Background",
+    
+    WindowShadow = Color3.new(0,0,0),
+    --WindowShadowTransparency = .7,
+    
+    WindowTopbarButtonIcon = "Icon",
+    WindowTopbarTitle = "Text",
+    WindowTopbarAuthor = "Text",
+    WindowTopbarIcon = "Text",
+    
+    TabBackground = "Hover",
+    TabTitle = "Text",
+    TabIcon = "Icon",
+    
+    ElementBackground = "Text",
+    ElementTitle = "Text",
+    ElementDesc = "Text",
+    ElementIcon = "Icon",
+    
+    PopupBackground = "Background",
+    PopupBackgroundTransparency = "BackgroundTransparency",
+    PopupTitle = "Text",
+    PopupContent = "Text",
+    PopupIcon = "Icon",
+    
+    DialogBackground = "Background",
+    DialogBackgroundTransparency = "BackgroundTransparency",
+    DialogTitle = "Text",
+    DialogContent = "Text",
+    DialogIcon = "Icon",
+    
+    
+}
